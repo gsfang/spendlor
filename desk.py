0bf9dc27fae12e690201f6eb1636e1c26f0e5912
gsfang <gsfang1204@gmail.com>
2017-04-07 18:35:00 +0800

    Add files via upload

---
+++ desk.py
@@ -0,0 +1,9 @@
+import csv
+class Hero():
+    def __init__(self,white,blue,green,red,black):
+            self.resource = [white, blue, green, red, black]
+    def __str__(self):
+            return self.resource 
+
+class Card():
+    def __init__ (self
